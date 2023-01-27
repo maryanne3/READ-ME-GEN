1b83461a7f898e3ebe5f29c7023d6031f2941240
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2023-01-27 20:35:55 +0000

    Added lists for licenses & Badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,35 +11,45 @@ function renderLicenseBadge(license)
   return  "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)]"
 } else if (license === licenseArray[3]){
   return  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]"
-} else {
-  return ""
-}
+} else { return "" }
 }
  
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === licenseArray [0]){
-  return `[${licenseArray[0]}](https://opensource.org/licenses/Apache-2.0)`
-} else if (license===licenseArray[1]){
-  return "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
-}  else if (license===licenseArray[2]){
-  return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-}  else if (license===licenseArray[3]){
-  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  if (license === licenseArray [0])
+{ return `[${licenseArray[0]}](https://opensource.org/licenses/Apache-2.0)`}
+else if  (license === licenseArray [1])
+{ return `[${licenseArray[1]}](https://opensource.org/licenses/BSD-2-Clause)`}
+else if  (license === licenseArray [2])
+{ return `[${licenseArray[2]}](https://opensource.org/licenses/BSD-3-Clause)`}
+else if  (license === licenseArray [3])
+{ return `[${licenseArray[3]}](https://opensource.org/licenses/MIT)`}
+else { return "" }
 }
-}
+  
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (license===licenseArray[0]){
+    return `Read more about ${licenseArray[0]} here:`
+  } else if (license===licenseArray[1]){
+    return `Read more about ${licenseArray[1]} here:`
+  } else if (license===licenseArray[2]){
+    return `Read more about ${licenseArray[2]} here:`
+  } else if (license===licenseArray[3]){
+    return `Read more about ${licenseArray[3]} here:`
+  }
+  else { return "" }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  
 
 `;
 }
